11f44e77721bcfc79473fc1dc262c276eeb2c531
deniz1717 <deniz.akyar123@gmail.com>
2025-03-04 22:33:10 +0300

    Delete bot_mantik.py

--- bot_mantik.py
+++
@@ -1,9 +0,0 @@
-import random
-import discord
-
-def gen_pass():
-    elements = "+-/*!&$#?=@<>qwertyuopasdfghjklizxcvbnmQWERTYUIOPASDFGHJKLZXCVBNNM"
-    password = ""
-    for i in range(10):
-        password += random.choice(elements)
-    return password
